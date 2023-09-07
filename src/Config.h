f455855234ded48e59322dcdd0d5737ddce97bb1
Craig Comberbach <craig.comberbach@gmail.com>
2023-09-07 17:15:46 -0500

    Cleaned up code
    main.c is not generic enough for inclusion in any project.
    Multi-spaces have been replaced with tabs.

--- src/Config.h
+++ src/Config.h
@@ -2,7 +2,7 @@
 #define	CONFIG_H
 
 //Hardware specific include file
-#include <p24f16KA10d1.h>
+#include <p24F16KA101.h>
 
 //Frequency Definitions
 enum FREQUENCY_UNITS
@@ -14,25 +14,22 @@ enum FREQUENCY_UNITS
 
 struct TIME
 {
-    uint16_t time;
-    enum TIME_UNITS
-    {
-        YEAR,
-        WEEK,
-        DAY,
-        HOUR,
-        MINUTE,
-        SECOND,
-        MILLISECOND,
-        MICROSECOND
-    } units;
+	uint16_t time;
+	enum TIME_UNITS
+	{
+		YEAR,
+		WEEK,
+		DAY,
+		HOUR,
+		MINUTE,
+		SECOND,
+		MILLISECOND,
+		MICROSECOND
+	} units;
 };
-//Project specific definition
-#define SCHEDULER_PERIOD    1
-#define SCHEDULER_UNITS     MILLISECOND
-#define FOSC_Hz             8000000
 
 //Helpful redefinitions
+#define FOSC_Hz			8000000
 #define FOSC_kHz		FOSC_Hz/1000
 #define FOSC_MHz		FOSC_kHz/1000
 #define FCY_Hz			FOSC_Hz/2
@@ -44,43 +41,44 @@ struct TIME
 //Pins
 enum PIN_DEFINITIONS
 {
-    PIN_RA0_PGC2,
-    PIN_RA1_PGD2,
-    PIN_RA2,
-    PIN_RA3,
-    PIN_RA4_PGC3,
-    PIN_RA5_MCLR,
-    PIN_RA6,
-    PIN_RB0_PGD1,
-    PIN_RB1_PGC1,
-    PIN_RB2,
-    PIN_RB4_PGD3,
-    PIN_RB7,
-    PIN_RB8,
-    PIN_RB9,
-    PIN_RB12,
-    PIN_RB13,
-    PIN_RB14,
-    PIN_RB15,
+	PIN_RA0_PGC2,
+	PIN_RA1_PGD2,
+	PIN_RA2,
+	PIN_RA3,
+	PIN_RA4_PGC3,
+	PIN_RA5_MCLR,
+	PIN_RA6,
+	PIN_RB0_PGD1,
+	PIN_RB1_PGC1,
+	PIN_RB2,
+	PIN_RB4_PGD3,
+	PIN_RB7,
+	PIN_RB8,
+	PIN_RB9,
+	PIN_RB12,
+	PIN_RB13,
+	PIN_RB14,
+	PIN_RB15,
 	NUMBER_OF_PINS
 };
 
-//Expected Library Versions
-    //Pins Library
-    #define PINS_MAJOR	2
-    #define PINS_MINOR	0
-    #define PINS_PATCH	0
-
-    //Scheduler Library
-    #define SCHEDULER_MAJOR	1
-    #define SCHEDULER_MINOR	0
-    #define SCHEDULER_PATCH	0
-
+//Scheduler
 enum SCHEDULER_DEFINITIONS
 {
 	TASK_,
 	NUMBER_OF_TASKS
 };
+
+//Expected Library Versions
+	//Pins Library
+	#define PINS_MAJOR	2
+	#define PINS_MINOR	0
+	#define PINS_PATCH	0
+
+	//Scheduler Library
+	#define SCHEDULER_MAJOR	1
+	#define SCHEDULER_MINOR	0
+	#define SCHEDULER_PATCH	0
 
 void Configure_MCU(void);
 
--- src/main.c
+++ src/main.c
@@ -1,33 +1,8 @@
-/*************    Header Files    ***************/
 #include "Config.h"
-#include "scheduler.h"
-
-/************* Library Definition ***************/
-/************* Semantic Versioning***************/
-/************Arbitrary Functionality*************/
-/*************   Magic  Numbers   ***************/
-/*************    Enumeration     ***************/
-/***********  Structure Definitions  ************/
-/***********State Machine Definitions************/
-/*************  Global Variables  ***************/
-/*************Function  Prototypes***************/
-/************* Device Definitions ***************/
-/************* Module Definitions ***************/
-/************* Other  Definitions ***************/
 
 int main()
 {
-    Initialize_Scheduler(SCHEDULER_PERIOD, SCHEDULER_UNITS);
 	Configure_MCU();
-
-	while(1)
-	{
-		while(Waiting_To_Run_Tasks())
-		{
-			asm("ClrWdt");
-			Task_Master();
-		}
-	}
 
 	return 1;
 }
