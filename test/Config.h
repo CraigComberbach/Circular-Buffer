d4494b5966d9f02500f0f81712377aa2dd80cc43
Craig Comberbach <craig.comberbach@gmail.com>
2023-11-19 14:03:52 -0600

    Started implementing tests

---
+++ test/Config.h
@@ -0,0 +1,16 @@
+#ifndef Config_H
+#define Config_H
+enum CircularBuffer_ObjectList
+{
+	CB_GLOBAL_TEST,
+	CB_LOCAL_TEST,
+	NUMBER_OF_CIRCULAR_BUFFERS
+};
+
+	//CircularBuffers Library
+	#define FEATURE_CIRCULAR_BUFFER
+	#define CIRCULAR_BUFFER_MAJOR	0
+	#define CIRCULAR_BUFFER_MINOR	0
+	#define CIRCULAR_BUFFER_PATCH	0
+
+#endif//Config_H
--- test/test_CircularBuffer.c
+++ test/test_CircularBuffer.c
@@ -2,20 +2,100 @@
 #ifdef TEST
 
 #include "unity.h"
-
+#include <stddef.h>
+#include <stdint.h>
 #include "CircularBuffer.h"
+
+#define GLOBAL_BUFFER_SIZE  8
+CircularBuffer_t *GlobalHandle = NULL;
+uint8_t GlobalBuffer[GLOBAL_BUFFER_SIZE];
 
 void setUp(void)
 {
+    CB_Aquire_CircularBuffer_Object(&GlobalHandle, CB_GLOBAL_TEST);
+    CB_Reset_CircularBuffer(GlobalHandle);
+    CB_Set_Buffer(GlobalHandle, &GlobalBuffer[0], GLOBAL_BUFFER_SIZE);
+    
+    return;
 }
 
 void tearDown(void)
 {
+    CB_Return_CircularBuffer_Object(&GlobalHandle);
 }
 
-void test_CircularBuffer_NeedToImplement(void)
+void test_CircularBuffer_Aquire_And_Release_Handle(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement CircularBuffer");
+    CircularBuffer_t *LocalHandle = NULL;
+    uint8_t ReturnCode;
+
+    ReturnCode = CB_Aquire_CircularBuffer_Object(&LocalHandle, CB_LOCAL_TEST);
+    TEST_ASSERT_EQUAL_INT (1, ReturnCode);
+    TEST_ASSERT_NOT_NULL(LocalHandle);
+
+    ReturnCode = CB_Return_CircularBuffer_Object(&LocalHandle);
+    TEST_ASSERT_EQUAL_INT (1, ReturnCode);
+    TEST_ASSERT_NULL(LocalHandle);
+    
+    return;
+}
+
+void test_CircularBuffer_Write_From_Empty_To_Full(void)
+{
+    uint8_t Data[] = {1,2,3};
+    uint8_t ReturnCode;
+    uint8_t Loop;
+
+    for(Loop = 0; Loop < (GLOBAL_BUFFER_SIZE-1); Loop++)
+    {
+        ReturnCode = CB_Write(GlobalHandle, &Data[0]);
+        TEST_ASSERT_EQUAL_INT (1, ReturnCode);
+    }
+
+    ReturnCode = CB_Write(GlobalHandle, &Data[0]);
+    TEST_ASSERT_EQUAL_INT (0, ReturnCode);
+
+    return;
+}
+
+void test_CircularBuffer_Read_From_Full_To_Empty(void)
+{
+    uint8_t Data[] = {1,2,3};
+    uint8_t ReturnCode;
+    uint8_t Loop;
+
+    for(Loop = 0; Loop < (GLOBAL_BUFFER_SIZE-1); Loop++)
+    {
+        ReturnCode = CB_Write(GlobalHandle, &Data[0]);
+    }
+
+    for(Loop = 0; Loop < (GLOBAL_BUFFER_SIZE-1); Loop++)
+    {
+        ReturnCode = CB_Read(GlobalHandle, &Data);
+        TEST_ASSERT_EQUAL_INT (1, ReturnCode);
+    }
+
+    ReturnCode = CB_Read(GlobalHandle, &Data);
+    TEST_ASSERT_EQUAL_INT (0, ReturnCode);
+
+    return;
+}
+
+void test_CircularBuffer_Write_Read_Validity(void)
+{
+    uint8_t Data[] = {1,2,3};
+    uint8_t *ReturnedData;
+    uint8_t ReturnCode;
+
+    TEST_IGNORE();
+    ReturnCode = CB_Write(GlobalHandle, &Data[0]);
+    TEST_ASSERT_EQUAL_INT (1, ReturnCode);
+    
+    ReturnCode = CB_Read(GlobalHandle, &ReturnedData);
+    TEST_ASSERT_EQUAL_INT (1, ReturnCode);
+    TEST_ASSERT_EQUAL_PTR(Data, *ReturnedData);
+
+    return;
 }
 
 #endif // TEST
